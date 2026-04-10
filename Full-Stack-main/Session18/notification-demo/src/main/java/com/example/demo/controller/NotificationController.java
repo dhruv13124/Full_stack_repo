4ecc48b6ae16dadc0e9872b23424e97876cc2d76
DHRUV <dhruvjangid522@gmail.com>
2026-04-10 23:30:23 +0530

    Add files via upload

---
+++ Full-Stack-main/Session18/notification-demo/src/main/java/com/example/demo/controller/NotificationController.java
@@ -0,0 +1,27 @@
+package com.example.demo.controller;
+
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.*;
+
+import com.example.demo.service.NotificationService;
+
+@RestController
+@RequestMapping("/notify")
+public class NotificationController {
+
+    private NotificationService notificationService;
+
+    @Autowired
+    public NotificationController(
+            @Qualifier("emailService") NotificationService notificationService) {
+        this.notificationService = notificationService;
+    }
+
+    @GetMapping
+    public String sendNotification(@RequestParam String message) {
+        return notificationService.sendNotification(message);
+    }
+}
